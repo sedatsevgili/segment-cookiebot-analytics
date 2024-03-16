7438034879dab3fdb407845aa04e0f0a58f1cf1a
Sedat <sedatsevgili@gmail.com>
2024-03-17 00:23:43 +0300

    set consent instead of destination filtering

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,13 @@
+
+
 declare global {
+    type NextFunction = (payload: any) => any;
+    type Middleware = ({payload: any, next: NextFunction}) => any;
     interface Window {
         analytics: {
             register: (plugin: any) => void;
             page: () => void;
+            addSourceMiddleware: (middleware: Middleware) => void;
         };
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,99 +4,29 @@ const segmentCookieBotAnalyticsWrapper = async (): Promise<void> => {
         return;
     }
 
-    const segmentKey = "<YOUR_SEGMENT_KEY>";
-    const destinationCategoryMapping: Record<string, string> = {
-        CRM: "preferences",
-        "Customer Success": "preferences",
-        "Feature Flagging": "preferences",
-        "A/B Testing": "statistics",
-        Analytics: "statistics",
-        Attribution: "statistics",
-        "Heatmaps & Recordings": "statistics",
-        "Performance Monitoring": "statistics",
-        "Tag Managers": "statistics",
-        Advertising: "marketing",
-        "Deep Linking": "marketing",
-        Email: "marketing",
-        "Email Marketing": "marketing",
-        Livechat: "marketing",
-        "Marketing Automation": "marketing",
-        Personalization: "marketing",
-        "Raw Data": "marketing",
-        Referrals: "marketing",
-        "Security & Fraud": "marketing",
-        "SMS & Push Notifications": "marketing",
-        Surveys: "marketing",
-        Video: "marketing",
-    };
-
-    const segmentWrapper = async (): Promise<void> => {
-        const segmentConsent = true;
-        const defaultConsent = false;
-
-        /**
-         * Fetch destinations from Segment
-         */
-        const destinations = await fetchDestinations();
-
+    const cookieBotMiddleware:Middleware = ({ payload, next}) => {
         const cookieConsent = getCookie("CookieConsent");
         if (null === cookieConsent) {
-            window.analytics.page();
-            return;
+            return next(payload);
         }
 
         const consent = JSON.parse(convertToSafeJSON(cookieConsent));
         if (!consent) {
-            window.analytics.page();
-            return;
+            return next(payload);
         }
 
-        let destinationPreferences: Record<string, boolean> = {
-            "Segment.io": segmentConsent,
-        };
-        for (let destination of destinations) {
-            const destinationCategory = destination.category;
-            if (typeof destinationCategory !== "string") {
-                destinationPreferences[destination.name] = defaultConsent;
-                continue;
-            }
-            if (!destinationCategoryMapping[destinationCategory]) {
-                destinationPreferences[destination.name] = defaultConsent;
-                continue;
-            }
-            const consentCategory =
-                destinationCategoryMapping[destinationCategory];
-            destinationPreferences[destination.name] = consent[consentCategory];
-        }
-
-        const plugin = {
-            name: "Cookiebot Consent",
-            type: "enrichment",
-            version: "1.0.0",
-            isLoaded: () => true,
-            load: () => Promise.resolve(),
-            page: (ctx: any) => {
-                ctx.updateEvent(
-                    (ctx.event.integrations = destinationPreferences),
-                );
-            },
-            track: (ctx: any) => {
-                ctx.updateEvent(
-                    (ctx.event.integrations = destinationPreferences),
-                );
-            },
-            identify: (ctx: any) => {
-                ctx.updateEvent(
-                    (ctx.event.integrations = destinationPreferences),
-                );
+        payload.obj.context = {
+            ...payload.obj.context,
+            consent: {
+                categoryPreferences: {
+                    marketing: consent.marketing || false,
+                    necessary: consent.necessary || false,
+                    preferences: consent.preferences || false,
+                    statistics: consent.statistics || false,
+                },
             },
         };
-
-        /**
-         * Add source middleware
-         */
-        window.analytics.register(plugin);
-        window.analytics.page();
+        return next(payload);
     };
 
     const getCookie = (key: string): string | null => {
@@ -120,21 +50,8 @@ const segmentCookieBotAnalyticsWrapper = async (): Promise<void> => {
         return cookie.replace(/'/g, '"').replace(/(\w+):/g, '"$1":');
     };
 
-    const fetchDestinations = async (): Promise<
-        { name: string; category: string }[]
-    > => {
-        const response = await fetch(
-            `https://cdn.segment.io/v1/projects/${segmentKey}/integrations`,
-        );
-        const data = await response.json();
-
-        return data.map((destination: any) => ({
-            name: destination.name,
-            category: destination.category,
-        }));
-    };
-
-    segmentWrapper();
+    window.analytics.addSourceMiddleware(cookieBotMiddleware);
+    window.analytics.page();
 };
 
 segmentCookieBotAnalyticsWrapper();
